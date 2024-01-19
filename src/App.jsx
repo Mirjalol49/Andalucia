b592d1236a9ece2da7995246c2c41cd2cd0ef777
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-01-20 00:36:59 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-comment-textnodes */
+
+function App() {
+  return (
+    <>
+      <h1>Andalucia o'quv markazi rasmiy websitiga xush kelibsiz</h1>
+    </>
+  );
+}
+
+export default App;
