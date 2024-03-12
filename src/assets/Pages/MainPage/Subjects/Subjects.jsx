f54d0c15dff1303bb5ac53bedd28a1e2869d8e8f
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-03-12 16:32:23 +0500

    size changed for iphone size

--- src/assets/Pages/MainPage/Subjects/Subjects.jsx
+++ src/assets/Pages/MainPage/Subjects/Subjects.jsx
@@ -22,56 +22,56 @@ const Subjects = () => {
           <SubCard
             subImg={Arabic}
             subject={"Arab Tili"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#0BA95B"}
           />
 
           <SubCard
             subImg={Mental}
             subject={"Mental Arifmetika"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#9163e0"}
           />
 
           <SubCard
             subImg={Onatili}
             subject={"Ona tili"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#12B5E5"}
           />
 
           <SubCard
             subImg={Ingliz}
             subject={"Ingliz tili"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#4ec5c1"}
           />
 
           <SubCard
             subImg={Math}
             subject={"Matematika"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#ED203D"}
           />
 
           <SubCard
             subImg={Turk}
             subject={"Turk tili"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#FB7428"}
           />
 
           <SubCard
             subImg={Russian}
             subject={"Rus tili"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#F38BA3"}
           />
 
           <SubCard
             subImg={Pochemuchka}
             subject={"Pochemuchka"}
-            subtitle={`Oq'uvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
+            subtitle={`O'quvchilarimiz qisqa mudatda CEFR imtixonida kamida B2 sertifikat olishadi.`}
             bg={"#FCBA28"}
           />
         </div>
